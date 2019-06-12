a7181edbba97538d103a4d896a02c2e771df4307
fo0s <fo0sies@gmail.com>
2019-06-12 13:50:17 +0100

    Refactor: Method crunching times can be displayed on a sinatra parsed page

--- lib/build_graphs.rb
+++ lib/build_graphs.rb
@@ -9,22 +9,71 @@ def build_graph
                 4000, 5000, 5500, 6000, 6500, 7000, 7500,
                 8000, 8500, 9000, 9500, 10000]
 
-  @results = []
-
-  puts "Beginning stress tests, please wait..."
+  @my_reverse_results = []
+  @my_shuffle_results = []
+  @my_fib_results = []
+  @my_bubble_results = []
+  @my_quick_results = []
 
   # Flushing CPU
   Timer.time { 10000.my_reverse }
 
   # Building datasets
+
+  # my_reverse integer
   @datapoints.each do |datapoint|
     temp_data = []
-    10.times do
+    500.times do
       Timer.time { datapoint.my_reverse }
       temp_data << Timer.elapsedTime
     end
-    @results << (temp_data.sum / 10)
+    @my_reverse_results << (temp_data.sum / 500)
   end
+
+  # my_shuffle
+  @datapoints.each do |datapoint|
+    temp_data = []
+    500.times do
+      Timer.time { datapoint.my_shuffle }
+      temp_data << Timer.elapsedTime
+    end
+    @my_shuffle_results << (temp_data.sum / 500)
+  end
+
+  # my_fib
+  @datapoints.each do |datapoint|
+    temp_data = []
+    10.times do
+      Timer.time { datapoint.my_fib }
+      temp_data << Timer.elapsedTime
+    end
+    @my_fib_results << (temp_data.sum / 10) / 500
+  end
+
+  # my_bubble_sort after my_shuffle
+
+  @datapoints.each do |datapoint|
+    temp_data = []
+    5.times do
+      Timer.time { 
+        data = datapoint.my_shuffle.to_s.split("")
+        data.my_bubble_sort }
+      temp_data << Timer.elapsedTime
+    end
+    @my_bubble_results << (temp_data.sum / 5)
+  end
+
+    @datapoints.each do |datapoint|
+    temp_data = []
+    5.times do
+      Timer.time { 
+        data = datapoint.my_shuffle.to_s.split("")
+        data.my_quick_sort }
+      temp_data << Timer.elapsedTime
+    end
+    @my_quick_results << (temp_data.sum / 5)
+  end
+
 
   # Create a new line chart
   chart = PiCharts::Line.new
@@ -35,7 +84,12 @@ def build_graph
                   "8000", "8500", "9000", "9500", "10000"])
 
   # Add datasets
-  chart.add_dataset(label: "Reverse", data: @results)
+  chart.add_dataset(label: "my_reverse", data: @my_reverse_results)
+  chart.add_dataset(label: "my_shuffle", data: @my_shuffle_results)
+  chart.add_dataset(label: "my_fib (divided by 500)", data: @my_fib_results)
+  chart.add_dataset(label: "my_bubble_sort", data: @my_bubble_results)
+  chart.add_dataset(label: "my_quick_sort", data: @my_quick_results)
+
 
   # Neat 'lil configurations
   chart.hover
